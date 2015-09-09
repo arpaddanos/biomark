7bd701ae0f2fa350d4313c2a77b7165aefb5cd65
arpaddanos <arpaddanos@gmail.com>
2015-09-09 03:47:36 -0500

    biomark on proven dataset branch

--- biomark.R
+++ biomark.R
@@ -11,15 +11,42 @@ vamp=sample(1:nrow(dunn),139)
 rr=c(1:nrow(dunn))
 tet=setdiff(rr,vamp)
 
+setwd("C:/R/biomark")
+
 fdunn=dunn
 fdunn$L.category.numeric=as.factor(fdunn$L.category.numeric)
 
 #Adjust A as desired to get a larger or smaller amount of genes (variable number)
 
-X=t(fdunn[,8:18653])
-ffun=filterfun(pOverA(p = 0.2, A = 10), cv(a = 0.7, b = 10))
+#setwd("/Users/ogriffit/git/biostar-tutorials/MachineLearning")
+datafile="trainset_gcrma.txt" 
+#clindatafile="trainset_clindetails.txt"
+#outfile="trainset_RFoutput.txt"
+#varimp_pdffile="trainset_varImps.pdf"
+#MDS_pdffile="trainset_MDS.pdf"
+#ROC_pdffile="trainset_ROC.pdf"
+#case_pred_outfile="trainset_CasePredictions.txt"
+#vote_dist_pdffile="trainset_vote_dist.pdf"
+
+data_import=read.table(datafile, header = TRUE, na.strings = "NA", sep="\t")
+#clin_data_import=read.table(clindatafile, header = TRUE, na.strings = "NA", sep="\t")
+#clin_data_order=order(clin_data_import[,"GEO.asscession.number"])
+#clindata=clin_data_import[clin_data_order,]
+#data_order=order(colnames(data_import)[4:length(colnames(data_import))])+3 #Order data without first three columns, then add 3 to get correct index in original file
+#rawdata=data_import[,c(1:3,data_order)] #grab first three columns, and then remaining columns in order determined above
+#header=colnames(rawdata)
+
+fdunn=as.data.frame(t(data_import[,4:length(colnames(data_import))]))
+
+X=t(fdunn)
+ffun=filterfun(pOverA(p = 0.2, A = 100), cv(a = 0.7, b = 10))
 filt=genefilter(2^X,ffun)
 dunnf=as.data.frame(t(X[filt,]))
+
+vamp=sample(1:nrow(dunn),139)
+rr=c(1:nrow(dunn))
+tet=setdiff(rr,vamp)
+
 dunnf$AGE=dunn$AGE
 dunnf1=dunnf
 dunnf2=dunnf
